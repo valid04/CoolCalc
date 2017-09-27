1de159e9f5218605802f362375cab65c48abdcb8
Niklas <nhojerslev@hotmail.com>
2017-09-27 13:13:19 +0200

    stag1

--- app/src/main/java/eu/injust/coolcalc/CalcActivity.java
+++ app/src/main/java/eu/injust/coolcalc/CalcActivity.java
@@ -1,9 +1,9 @@
 package eu.injust.coolcalc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class CalcActivity extends AppCompatActivity {
+public class CalcActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
